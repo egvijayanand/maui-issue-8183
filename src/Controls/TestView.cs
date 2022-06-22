a228b93a781eb7ba032e88a3d74e4428405a5d41
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-06-23 01:57:03 +0530

    Simulation source

---
+++ src/Controls/TestView.cs
@@ -0,0 +1,14 @@
+﻿namespace Issue8183.Controls
+{
+    public class TestView : View
+    {
+        public static readonly BindableProperty TextProperty
+            = BindableProperty.Create(nameof(Text), typeof(string), typeof(TestView), string.Empty);
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+    }
+}
---
+++ src/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Issue8183
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
